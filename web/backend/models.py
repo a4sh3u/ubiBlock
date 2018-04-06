cecc6f869fcec43d92edd1dccdca0f1caa54ba85
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-04-06 16:34:30 +0200

    login working

--- web/backend/models.py
+++ web/backend/models.py
@@ -4,15 +4,7 @@ from django.db.models import CharField
 
 class CustomerAccounts(models.Model):
     id = models.AutoField(primary_key=True)
-    mail_id = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    create_date = models.DateTimeField('date published')
+    username = models.CharField(max_length=200)
     account_id = models.CharField(max_length=200)
     account_password = models.CharField(max_length=200)
     account_balance = models.CharField(max_length=200)
-
-class Transactions(models.Model):
-    txn_id = models.AutoField(primary_key=True)
-    txn_hash = models.CharField(max_length=200)
-    txn_from_address = models.CharField(max_length=200)
-    txn_value = models.CharField(max_length=200)
